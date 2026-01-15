687721460bdec3972d43f7541906f20f792cfae0
Harsha-jain11 <hhjain156@gmail.com>
2026-01-15 23:23:11 +0530

    Update cube.py

--- cube.py
+++ cube.py
@@ -4,9 +4,12 @@ import sys
 def cube_of_number(num):
     return num ** 3
 
-if len(sys.argv) != 2:
-    print("Usage: python cube.py <number>")
-    sys.exit(1)
 
-num = int(sys.argv[1])
-print("The cube of number is:", cube_of_number(num))
+if __name__ == "__main__":
+
+    if len(sys.argv) != 2:
+        print("Usage: python cube.py <number>")
+        sys.exit(1)
+
+    num = int(sys.argv[1])
+    print("The cube of number is:", cube_of_number(num))
